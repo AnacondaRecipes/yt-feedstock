20ac65777264e15f6cff87469590ec048cc33724
Eric Lundby <Eric.Lundby@gmail.com>
2023-04-03 15:29:15 -0600

    Remove build scripts

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(uname) == Darwin ]]; then
-  # make sure libomp can be found by setupexp.py ...
-  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PREFIX}/lib"
-fi
-
-$PYTHON setup.py -q install --single-version-externally-managed --record=record.txt
-
